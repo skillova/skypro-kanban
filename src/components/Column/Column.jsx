69d465cf2048a997db9d228959c3d0774ecd539d
skillova <skillova@yandex.ru>
2026-04-04 23:45:00 +0300

    Декомпозиция: минимум абы какие 8 компонетов.

---
+++ src/components/Column/Column.jsx
@@ -0,0 +1,11 @@
+import Card from "../Card/Card";
+
+function Column() {
+  return (
+    <div className="main__content">
+        <Card />
+    </div>
+  );
+}
+
+export default Column;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import Column from "../Column/Column";
+
+function Main() {
+  return (
+    <main className="main">
+      <div className="container">
+        <div className="main__block">
+          <Column />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
