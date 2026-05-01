29e8fbc581a771b5839fd6d8846cbd4b76b1b7d8
skillova <skillova@yandex.ru>
2026-05-01 16:15:00 +0300

    Рендер карточек из массива

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,10 +1,27 @@
-import Card from "../Card/Card";
+import Cards from "../Card/Card";
 
 function Column() {
-  return (
-    <div className="main__content">
-        <Card />
+  const statuses = [
+    "Без статуса",
+    "Нужно сделать",
+    "В работе",
+    "Тестирование",
+    "Готово",
+  ];
+  const columns = statuses.map((status) => (
+    <div className="main__column">
+      <div className="column__title">
+        <p>{status}</p>
+      </div>
+      <div className="cards">
+        <Cards status={status} key={status} />
+      </div>
     </div>
+  ));
+  return (
+    <>
+      <div className="main__content">{columns}</div>
+    </>
   );
 }
 
