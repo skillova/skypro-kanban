2504d9af1f525daf5dcabc1f1e1f122d3c3a8a66
skillova <skillova@yandex.ru>
2026-05-02 15:57:52 +0300

    имитация загрузки данных

---
+++ src/components/Main/Loading.jsx
@@ -0,0 +1,8 @@
+function Loading() {
+  return (
+    <div className="loading">
+      <p>Загрузка данных...</p>
+    </div>
+  );
+}
+export default Loading;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,22 +1,35 @@
+import { useState, useEffect } from "react";
 import Column from "../Column/Column";
+import Loading from "./Loading";
 import { cardsList, statuses } from "../../data";
 
 function Main() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [localCardsList] = useState(cardsList);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <main className="main">
       <div className="container">
         <div className="main__block">
-          <div class="main__content">
-            {statuses.map((status) => (
-              <Column
-                key={status}
-                title={status}
-                cardsObjList={cardsList.filter(
-                  (cardObj) => cardObj.status === status,
-                )}
-              />
-            ))}
-          </div>
+          {isLoading && <Loading />}
+          {!isLoading && (
+            <div className="main__content">
+              {statuses.map((status) => (
+                <Column
+                  key={status}
+                  title={status}
+                  cardsObjList={localCardsList.filter(
+                    (cardObj) => cardObj.status === status,
+                  )}
+                />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </main>
